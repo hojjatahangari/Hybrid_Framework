3ab3d4cdd6bbbe413adfa2486a33183b1ea72624
hojjatahangari <hojjat.ahangari@yahoo.com>
2023-12-21 11:55:43 -0500

    Second attempt to push my framework

---
+++ src/test/java/com/tutorialsninja_testcases/StudentInfo.java
@@ -0,0 +1,14 @@
+package com.tutorialsninja_testcases;
+
+import org.testng.annotations.Test;
+
+public class StudentInfo {
+@Test
+public void myInfo() {
+	System.out.println("Name:	    Hojjatollah Ahangarikiasari");
+	System.out.println("Student ID: 20644VA");
+	System.out.println("Email:      ahangari.hojjat@gmail.com");
+	System.out.println("Telephone#: 2023902062");
+}
+}
+   
